6417f86206cbc8b99aaa1820e39740ba4b772c12
Ubuntu <ubuntu@ip-172-31-24-82.eu-west-2.compute.internal>
2024-03-12 03:01:11 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const mysql = require('mysql');
+const fs = require('fs');
+const stringify = require('csv-stringify');
+
+const connection = mysql.createConnection({
+  host: 'demodatabase.c9iggaio00lb.eu-west-2.rds.amazonaws.com',
+  user: 'demoUsername',
+  password: 'demoPassword',
+  database: 'ExampleDB'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+
+  const createTableSql = `CREATE TABLE IF NOT EXISTS Customer (
+    CustID INT PRIMARY KEY,
+    Name VARCHAR(255)
+  )`;
+  connection.query(createTableSql, (err, result) => {
+    if (err) throw err;
+    console.log('Table created');
+
+    const insertSql = `INSERT INTO Customer (CustID, Name) VALUES (?, ?)`;
+    const randomId = Math.floor(Math.random() * 1000) + 1;
+    const randomName = `Customer${randomId}`;
+    connection.query(insertSql, [randomId, randomName], (err, result) => {
+      if (err) throw err;
+      console.log('Record inserted');
+
+      const selectSql = 'SELECT * FROM Customer';
+      connection.query(selectSql, (err, rows) => {
+        if (err) throw err;
+
+        let mdContent = '# Customer Data\n\n';
+        // Add table headers
+        mdContent += '| CustID | Name |\n';
+        mdContent += '|--------|------|\n';
+        rows.forEach((row) => {
+          mdContent += `| ${row.CustID} | ${row.Name} |\n`;
+        });
+
+        fs.writeFile('output.md', mdContent, (err) => {
+          if (err) throw err;
+          console.log('The file has been saved!');
+        });
+
+        connection.end();
+      });
+    });
+  });
+});
---
+++ output.md
@@ -0,0 +1,14 @@
+# Customer Data
+
+| CustID | Name |
+|--------|------|
+| 62 | Customer62 |
+| 444 | Customer444 |
+| 485 | Customer485 |
+| 614 | Customer614 |
+| 747 | Customer747 |
+| 865 | Customer865 |
+| 867 | Customer867 |
+| 910 | Customer910 |
+| 976 | Customer976 |
+| 1021 | Martha Rivera |
